d50fe498bf1a660687337adcc51b1b6ffa950c9e
Stephane Macedo <stephanemacedo1@gmail.com>
2022-02-04 12:00:19 -0300

    Adicionar arquivos de projeto.

---
+++ Diferencas/Program.cs
@@ -0,0 +1,30 @@
+﻿//Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D 
+//segundo a fórmula: DIFERENCA = (A * B - C * D).
+
+//Entrada
+//O arquivo de entrada contém 4 valores inteiros.
+
+//Saída
+//Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, 
+//com um espaço em branco antes e depois da igualdade.
+
+using System;
+ 
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int A = int.Parse(Console.ReadLine());
+          int B = int.Parse(Console.ReadLine());
+          int C = int.Parse(Console.ReadLine());
+          int D = int.Parse(Console.ReadLine());
+         
+         int diferenca = (A * B) - (C * D);
+
+            Console.WriteLine($"DIFERENCA = {diferenca}");
+           
+        }
+    }
+}
